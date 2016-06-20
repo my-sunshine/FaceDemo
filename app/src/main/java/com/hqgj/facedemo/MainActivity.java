fa364aa78b0e4a801c0d1b6d5519c2c8684ae376
红姬 <3113126536@qq.com>
2016-06-20 18:08:14 +0800

    111

--- app/src/main/java/com/hqgj/facedemo/MainActivity.java
+++ app/src/main/java/com/hqgj/facedemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.hqgj.facedemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         faceContainerView= (FaceContainerView) findViewById(R.id.faceContainerView);
-
         faceContainerView.setOnSendMessageListener(new FaceContainerView.OnSendMessageListener() {
             @Override
             public void onSendMessageListener(String message) {
@@ -41,14 +41,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        faceContainerView.setOnTakePhotoListener(new FaceContainerView.OnTakePhotoListener() {
+            @Override
+            public void onTakePhotoListener() {
+                Toast.makeText(MainActivity.this,"onTakePhotoListener",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onTakeCameraListener() {
+                Toast.makeText(MainActivity.this,"onTakeCameraListener",Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         listView= (ListView) findViewById(R.id.listView);
-
         adapter=new ListViewAdapter(this,lists);
-
         listView.setAdapter(adapter);
 
 
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                faceContainerView.showKeyBoard();
+            }
+        });
+
+        findViewById(R.id.btnHidden).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                faceContainerView.hiddenKeyBoard();
+            }
+        });
 
     }
 }
